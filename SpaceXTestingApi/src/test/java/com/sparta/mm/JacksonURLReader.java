872dc174392cfb9495ab7acd9264e51ba7128a01
MahamudHub <mo.mahamud@outlook.com>
2020-07-02 13:54:52 +0100

    Jackson reader class updated

--- SpaceXTestingApi/src/test/java/com/sparta/mm/JacksonURLReader.java
+++ SpaceXTestingApi/src/test/java/com/sparta/mm/JacksonURLReader.java
@@ -4,24 +4,39 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class JacksonURLReader {
     ObjectMapper objectMapper = getDefaultObjectMapper();
 
-    public ObjectMapper getDefaultObjectMapper(){
+    private ObjectMapper getDefaultObjectMapper() {
         ObjectMapper defaultObjectMapper = new ObjectMapper();
         return defaultObjectMapper;
     }
 
-    public SpaceXPOJO readURLAsJSON (String path){
+    public SpaceXPOJO readURLAsJSON(String path) {
         SpaceXPOJO spaceXPOJO = new SpaceXPOJO();
         try {
             objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-            spaceXPOJO = objectMapper.readValue(new URL(path),SpaceXPOJO.class);
+            spaceXPOJO = objectMapper.readValue(new URL(path), SpaceXPOJO.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return spaceXPOJO;
+    }
+
+    public String getStatus(String path) {
+        String response = "";
+        try {
+            URL url = new URL(path);
+            URLConnection urlConnection = url.openConnection();
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            response = String.valueOf(httpURLConnection.getResponseCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
     }
 }
